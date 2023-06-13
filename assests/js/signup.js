f465c42afdca795bf340702f47530fc67b9d4cde
kubrajafar <7chpkku@code.edu.az>
2023-06-13 20:46:22 +0400

    update login

--- assests/js/signup.js
+++ assests/js/signup.js
@@ -1,5 +1,6 @@
 const signup_submit = document.querySelector(".signup-submit");
-
+const login_btn = document.querySelector(".login-btn");
+console.log(login_btn);
 const user = document.getElementById("user");
 const email = document.getElementById("email");
 const pass = document.getElementById("pass");
@@ -19,12 +20,12 @@ signup_submit.addEventListener("submit", (e) => {
     confPass: confPass.value,
   };
 
-
-
   arr.push(obj);
 
   localStorage.setItem("Data", JSON.stringify(arr));
 
   console.log(JSON.parse(localStorage.getItem("Data")));
+  window.location.href = "login.html";
 });
+
 // localStorage.clear()
